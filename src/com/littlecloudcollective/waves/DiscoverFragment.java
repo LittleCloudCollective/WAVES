c3b31d0b106da68660408a8e01ed9919e6d19224
Priyanka Pathak <priyankacpathak@gmail.com>
2014-12-07 23:37:25 -0500

    Removed TwoWayView; adding Maps
    -Reverted to an older commit, removed horizontal scrolling references
    -Added Google Play Services SDK
    -Redesigned Sound Fragment (though not displaying properly on phone)
    -Currently adding map to Discover tab

--- src/com/littlecloudcollective/waves/DiscoverFragment.java
+++ src/com/littlecloudcollective/waves/DiscoverFragment.java
@@ -1,7 +1,33 @@
 package com.littlecloudcollective.waves;
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class DiscoverFragment extends Fragment {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 
+public class DiscoverFragment extends SupportMapFragment {
+
+	private GoogleMap mGoogleMap;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_discover, parent, false);
+		
+		//Reference to the Google map
+		mGoogleMap = getMap();
+		
+		//Show the user's location
+		mGoogleMap.setMyLocationEnabled(true);
+		
+		return v;
+	}
+	
 }
